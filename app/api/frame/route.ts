c55113e38cf95984f37f27cb53457908fa440906
provoid228 <provoid228@gmail.com>
2024-03-09 23:34:04 +0300

    added new link

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -25,7 +25,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
    */
   if (message?.button === 3) {
     return NextResponse.redirect(
-      'https://www.google.com/search?q=cute+dog+pictures&tbm=isch&source=lnms',
+      'https://www.npmjs.com/package/@coinbase/onchainkit',
       { status: 302 },
     );
   }
