10a61d0f5ee41eabf703a31c73a10097324b3eaa
provoid228 <provoid228@gmail.com>
2024-03-09 23:42:33 +0300

    zazu sdulo

--- app/config.ts
+++ app/config.ts
@@ -1,4 +1,4 @@
 // use NODE_ENV to not have to change config based on where it's deployed
 export const NEXT_PUBLIC_URL =
-  process.env.NODE_ENV == 'development' ? 'http://localhost:3000' : 'https://zizzamia.xyz';
-export const BUY_MY_COFFEE_CONTRACT_ADDR = '0xcD3D5E4E498BAb2e0832257569c3Fd4AE439dD6f';
+  process.env.NODE_ENV == 'development' ? 'http://localhost:3000' : 'https://frameprovoid228.vercel.app/';
+export const BUY_MY_COFFEE_CONTRACT_ADDR = '0x7aC06C2b969e31ff155901743baC02D210a6A46B';
