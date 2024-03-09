9e254dc0e9a614c281add7ebff30f58268bd03a8
provoid228 <provoid228@gmail.com>
2024-03-09 23:46:18 +0300

    zaza time

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { NEXT_PUBLIC_URL } from './config';
 const frameMetadata = getFrameMetadata({
   buttons: [
     {
-      label: 'Story time',
+      label: 'Zzaza time',
     },
     {
       action: 'tx',
