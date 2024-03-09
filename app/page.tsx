b229821e05d915bd6f707e51c4463fafe11cfa2c
provoid228 <provoid228@gmail.com>
2024-03-09 23:49:12 +0300

    park-2

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ const frameMetadata = getFrameMetadata({
     },
   ],
   image: {
-    src: `${NEXT_PUBLIC_URL}/park-3.png`,
+    src: `${NEXT_PUBLIC_URL}/park-2.png`,
     aspectRatio: '1:1',
   },
   input: {
