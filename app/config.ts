7ea5ff3533d928c6315a7ea06dc193ce34950132
provoid228 <provoid228@gmail.com>
2024-03-10 15:00:35 +0300

    added adress

--- app/config.ts
+++ app/config.ts
@@ -1,4 +1,4 @@
 // use NODE_ENV to not have to change config based on where it's deployed
 export const NEXT_PUBLIC_URL =
   process.env.NODE_ENV == 'development' ? 'http://localhost:3000' : 'https://frameprovoid228.vercel.app/';
-export const BUY_MY_COFFEE_CONTRACT_ADDR = '0x7aC06C2b969e31ff155901743baC02D210a6A46B';
+export const BUY_MY_COFFEE_CONTRACT_ADDR = '0x30a75015a6745792E868D8D289089459275Edef9';
