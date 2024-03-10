ec3daf29dabf064c663118e7c1ba634a1c404991
provoid228 <provoid228@gmail.com>
2024-03-10 13:35:01 +0300

    added pec2

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -47,7 +47,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
         },
       ],
       image: {
-        src: `${NEXT_PUBLIC_URL}/park-1.png`,
+        src: `${NEXT_PUBLIC_URL}/pec2.png`,
       },
       postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
       state: {
